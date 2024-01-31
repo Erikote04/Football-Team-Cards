69141b29a7db8ef1cbfaffca129561a2fe719995
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 13:13:04 +0100

    Step 46

--- script.js
+++ script.js
@@ -207,5 +207,8 @@ playersDropdownList.addEventListener("change", (e) => {
         case "midfielder":
             setPlayerCards(players.filter((player) => player.position === "midfielder"));
             break;
+        case "defender":
+            setPlayerCards(players.filter((player) => player.position === "defender"));
+            break;
     }
 });
