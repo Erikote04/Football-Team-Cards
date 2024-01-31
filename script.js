5d0db27dd7f05bacaa449ef4a1a616f7ee8773d3
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 12:36:00 +0100

    Step 22

--- script.js
+++ script.js
@@ -176,3 +176,4 @@ const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
 
 typeOfSport.textContent = sport;
+teamName.textContent = team;
