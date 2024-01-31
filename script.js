d5ef57195f5cdc1f68a67176b85d7270a2b76960
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 10:55:12 +0100

    Step 6

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
 const myFavoriteFootballTeam = {
-    
+    team: "Argentina",
 };
