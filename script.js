59ff4ec849b910c0a993f800020cda0cb2f635ea
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 10:52:45 +0100

    Step 4

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
