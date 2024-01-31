4ee3bf43819096d2e3bf63a4fa729ae5590d6d99
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 12:46:54 +0100

    Step 29

--- script.js
+++ script.js
@@ -183,7 +183,11 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map(
         ({ name, position, number, isCaptain, nickname }) => {
-            `<div class="player-card"></div>`
+            `
+                <div class="player-card">
+                    <h2>${name}</h2>
+                </div>
+            `;
         }
     );
 };
